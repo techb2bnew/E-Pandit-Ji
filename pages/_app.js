d3452ebab616fb8bf74a0f67b4c60ed140458645
techb2bnew <tech@base2brand.com>
2026-04-29 15:56:42 +0530

    Home page design responsive all pages

--- pages/_app.js
+++ pages/_app.js
@@ -22,7 +22,7 @@ export default function App({ Component, pageProps }) {
         <
         Component {...pageProps }
         />  <
-        div className = "w-full bottom-0 z-10 fixed" >
+        div className = "w-full bottom-0 z-10 fixed block md:hidden" >
         <
         FooterMobileBar / >
         <
